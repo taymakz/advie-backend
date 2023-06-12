b9f6b3552d7c60f6e9f34f268b227ada15558551
TaymaZ <71381670+Taymaaz@users.noreply.github.com>
2023-06-13 01:48:31 +0330

    Vercel Deploy config

--- config/settings.py
+++ config/settings.py
@@ -161,7 +161,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
