be53936d8016802c2defd9304a17476aff20d834
TaymaZ <71381670+Taymaaz@users.noreply.github.com>
2023-06-13 12:01:12 +0330

    Vercel Deploy config

--- config/settings.py
+++ config/settings.py
@@ -175,7 +175,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CKEDITOR_UPLOAD_PATH = "media/contents/"
 
