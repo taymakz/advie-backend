9a3683fbc7b03bd541eaf42274cd1c42549295b7
TaymaZ <71381670+Taymaaz@users.noreply.github.com>
2023-06-13 12:09:42 +0330

    Vercel Deploy config

--- config/urls.py
+++ config/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('editor/', include('ckeditor_uploader.urls')),
 
 ]
-# if settings.DEBUG:
-#     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
