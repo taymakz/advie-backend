b79afdd0b67cc0712429c0e838ca8e306e45212c
TaymaZ <71381670+Taymaaz@users.noreply.github.com>
2023-06-13 11:46:55 +0330

    Vercel Deploy config

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
         'PORT': os.getenv('DATABASE_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
