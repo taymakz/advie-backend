bed8e276cc56a5cb09e7fbcf585f3008a94c50c6
TaymaZ <71381670+Taymaaz@users.noreply.github.com>
2023-06-14 23:18:24 +0330

    Vercel deploy settings

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,14 @@
+import os
+
 from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +17,5 @@ urlpatterns = [
 
 ]
 
-# if settings.DEBUG:
-#     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if os.environ.get('LOCAL_STORAGE') == 'True':
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
