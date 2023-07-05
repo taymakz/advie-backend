25142b7c91113d62db60b9b9ceea6e17e8a21605
Taymaz <taymazak1382@Gmail.com>
2023-07-06 01:25:47 +0330

    Payment Changes

--- site_shop/transaction_management/models.py
+++ site_shop/transaction_management/models.py
@@ -27,6 +27,8 @@ class Transaction(models.Model):
     status = models.CharField(max_length=20, choices=TRANSACTION_STATUS_CHOICES, blank=True, null=True)
     transaction_id = models.CharField(max_length=50, blank=True, null=True)
 
+    ref_id = models.CharField(max_length=255, blank=True, null=True)
+    reason = models.TextField(blank=True,null=True)
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
     date_updated = models.DateTimeField(auto_now=True, editable=False)
     is_delete = models.BooleanField(default=False)
--- site_shop/transaction_management/views.py
+++ site_shop/transaction_management/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
@@ -181,7 +182,7 @@ class RequestPaymentSubmitAPIView(APIView):
 
         data = {
             "MerchantID": settings.ZARINPAL_MERCHANT,
-            "Amount": amount * 10,
+            "Amount": order_total_price * 10,
             "Description": description,
             "Phone": user.phone,
             "CallbackURL": f"{CallbackURL}/api/payment/verify?order={current_order.slug}",
@@ -195,26 +196,47 @@ class RequestPaymentSubmitAPIView(APIView):
             if response.status_code == 200:
                 response = response.json()
                 if response['Status'] == 100:
-                    print(ZP_API_STARTPAY + str(response['Authority']))
-                    return BaseResponse(data={'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']),
-                                              'authority': response['Authority']}, status=status.HTTP_200_OK)
+                    current_order.coupon_effect_price = coupon_effect_dif_price
+                    current_order.shipping_effect_price = current_order.shipping.calculate_price(
+                        order_price=order_total_price_before_coupon)
+                    current_order.payment_status = PaymentStatus.PENDING_PAYMENT.name
+                    current_order.save()
+                    return BaseResponse(
+                        data={'is_free': False,
+                              'payment_gateway_link': ZP_API_STARTPAY + str(response['Authority'])},
+                        status=status.HTTP_200_OK,
+                        message='در حال پردازش')
                 else:
                     return BaseResponse(data={'status': False, 'code': str(response['Status'])})
             return response
 
         except requests.exceptions.Timeout:
-            return {'status': False, 'code': 'timeout'}
+            return BaseResponse(data={'status': False, 'code': 'خطای اتصال'})
         except requests.exceptions.ConnectionError:
-            return {'status': False, 'code': 'connection error'}
+            return BaseResponse(data={'status': False, 'code': 'خطای اتصال'})
 
 
 class VerifyPaymentAPIView(APIView):
 
     def get(self, request):
-        authority= request.GET.get('Authority', None)
+        order_slug = request.GET.get('order')
+
+        try:
+            current_order = Order.objects.get(slug=order_slug, payment_status=PaymentStatus.PENDING_PAYMENT.name)
+
+        except Order.DoesNotExist:
+            return BaseResponse(data={"redirect_to": f"{settings.FRONTEND_URL}/checkout/cart/"},
+                                status=status.HTTP_400_BAD_REQUEST,
+                                message=ResponseMessage.FAILED.value)
+        total_price = current_order.get_total_price
+        user = current_order.user
+        total_price -= current_order.coupon_effect_price
+        total_price += current_order.shipping_effect_price
+        authority = request.GET.get('Authority', None)
+
         data = {
             "MerchantID": settings.ZARINPAL_MERCHANT,
-            "Amount": amount,
+            "Amount": total_price * 10,
             "Authority": authority,
         }
         data = json.dumps(data)
@@ -224,8 +246,72 @@ class VerifyPaymentAPIView(APIView):
 
         if response.status_code == 200:
             response = response.json()
+
             if response['Status'] == 100:
-                return {'status': True, 'RefID': response['RefID']}
+                current_order.ordered_date = datetime.date.today()
+                current_order.payment_status = PaymentStatus.PAID.name
+                current_order.delivery_status = DeliveryStatus.PENDING.name
+                if current_order.coupon is not None:
+                    valid, message = current_order.coupon.validate_coupon(user_id=user.id,
+                                                                          order_total_price=total_price)
+                    if valid:
+                        new_price, dif_price = current_order.coupon.calculate_discount(current_order.get_total_price)
+                        current_order.coupon_effect_price = dif_price
+                    else:
+                        pass
+                for item in current_order.items.all():
+                    item.variant.stock -= item.count
+                    item.variant.save()
+                    item.set_final_price()
+                    item.save()
+                current_order.save()
+                Transaction.objects.create(user=user, order=current_order, status=TransactionStatus.SUCCESS.name,
+                                           ref_id=response['RefID'])
+                return redirect(f"{settings.FRONTEND_URL}/panel/")
+
+                # return {'status': True, 'RefID': response['RefID']}
             else:
-                return {'status': False, 'code': str(response['Status'])}
-        return response
+                errors = {
+                    1: 'اطلاعات ارسال شده ناقص است.',
+
+                    -2: 'IP و يا مرچنت كد پذيرنده صحيح نيست.',
+                    -3: 'با توجه به محدوديت هاي شاپرك امكان پرداخت با رقم درخواست شده ميسر نمي باشد.',
+                    -4: 'سطح تاييد پذيرنده پايين تر از سطح نقره اي است.',
+                    -11: 'درخواست مورد نظر يافت نشد.',
+                    -12: 'امكان ويرايش درخواست ميسر نمي باشد.',
+                    -21: 'هيچ نوع عمليات مالي براي اين تراكنش يافت نشد.',
+                    -22: 'تراكنش نا موفق ميباشد.',
+                    -33: 'رقم تراكنش با رقم پرداخت شده مطابقت ندارد.',
+                    -34: 'سقف تقسيم تراكنش از لحاظ تعداد يا رقم عبور نموده است.',
+                    -40: 'اجازه دسترسي به متد مربوطه وجود ندارد.',
+                    -41: 'اطلاعات ارسال شده مربوط به AdditionalData غيرمعتبر ميباشد.',
+                    -42: 'مدت زمان معتبر طول عمر شناسه پرداخت بايد بين 30 دقيه تا 45 روز مي باشد.',
+                    -54: 'درخواست مورد نظر آرشيو شده است.',
+
+                    2: 'IP و يا مرچنت كد پذيرنده صحيح نيست.',
+                    3: 'با توجه به محدوديت هاي شاپرك امكان پرداخت با رقم درخواست شده ميسر نمي باشد.',
+                    4: 'سطح تاييد پذيرنده پايين تر از سطح نقره اي است.',
+                    11: 'درخواست مورد نظر يافت نشد.',
+                    12: 'امكان ويرايش درخواست ميسر نمي باشد.',
+                    21: 'هيچ نوع عمليات مالي براي اين تراكنش يافت نشد.',
+                    22: 'تراكنش نا موفق ميباشد.',
+                    33: 'رقم تراكنش با رقم پرداخت شده مطابقت ندارد.',
+                    34: 'سقف تقسيم تراكنش از لحاظ تعداد يا رقم عبور نموده است.',
+                    40: 'اجازه دسترسي به متد مربوطه وجود ندارد.',
+                    41: 'اطلاعات ارسال شده مربوط به AdditionalData غيرمعتبر ميباشد.',
+                    42: 'مدت زمان معتبر طول عمر شناسه پرداخت بايد بين 30 دقيه تا 45 روز مي باشد.',
+                    54: 'درخواست مورد نظر آرشيو شده است.',
+
+                    100: 'عمليات با موفقيت انجام گرديده است.',
+                    101: 'عمليات پرداخت موفق بوده و قبلا PaymentVerification تراكنش انجام شده است.'
+                }
+                error_code = response['Status']
+                error_message = errors.get(error_code, 'خطای ناشناخته لطفا با پشتیبانی تماس بگیرید')
+                reason = f"Status: False,کد: {str(error_code)}, پیام: {error_message}"
+                Transaction.objects.create(user=user, order=current_order, status=TransactionStatus.FAILED.name,
+                                           reason=reason)
+                return redirect(f"{settings.FRONTEND_URL}/vf/pm?f={error_message}")
+
+        Transaction.objects.create(user=user, order=current_order, status=TransactionStatus.FAILED.name,
+                                   reason=response)
+        return redirect(f"{settings.FRONTEND_URL}/vf/pm?f=خطای ناشناخته لطفا با پشتیبانی تماس بگیرید")
